5b8e3430c947e24e478b10b641b855fc28c16e75
shashankbharadwaj27 <shashankbharadwaj2703@gmail.com>
2025-07-28 18:39:28 +0530

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Typewriter } from 'react-simple-typewriter';
-import resume from '../assets/Shashank_Resume_compressed.pdf'
+import resume from '../assets/Shashank_Resume_compressed.pdf';
 
 export default function Hero() {
   return (
     <section
       id="home"
-      className="min-h-screen flex flex-col-reverse md:flex-row justify-center items-center px-2 pt-28 md:pt-32 gap-10 md:gap-20"
+      className="min-h-screen flex flex-col-reverse md:flex-row justify-center items-center px-4 pt-28 md:pt-32 gap-10 md:gap-20"
     >
-      <div className="max-w-6xl text-center md:text-left">
-        <h2 className="text-4xl md:text-4xl font-bold mb-4">
-          <span className="text-cyan-400">
+      <div className="max-w-6xl w-full text-center md:text-left scale-[0.8] origin-top transform">
+        <h2 className="text-xl text-center sm:text-3xl md:text-4xl font-bold mb-4 whitespace-nowrap overflow-hidden text-ellipsis">
+          <span className="text-cyan-400 inline-block">
             <Typewriter
               words={['👋 Hello, I am Shashank Bharadwaj']}
               loop={3}
@@ -22,10 +22,14 @@ export default function Hero() {
             />
           </span>
         </h2>
-        <p className="text-lg mb-2 text-center text-gray-300" >MERN Stack developer | Machine Learning Practitioner</p>
-        <p className="text-lg text-center text-gray-300">
+
+        <p className="text-base sm:text-md mb-2 text-center text-gray-300 ">
+          MERN Stack developer | Machine Learning Practitioner
+        </p>
+        <p className="text-base sm:text-md text-center text-gray-300">
           Fuelled by curiosity, driven by code.
         </p>
+
         <div className="flex justify-center space-x-4 mt-6 text-2xl">
           <a
             href="https://github.com/shashankbharadwaj27"
@@ -44,6 +48,7 @@ export default function Hero() {
             <FaLinkedin className="hover:text-gray-600" />
           </a>
         </div>
+
         <div className="flex justify-center">
           <a
             href={resume}
